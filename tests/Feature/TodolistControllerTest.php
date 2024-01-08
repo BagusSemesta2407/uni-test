8e5b2d4d0c5b7cedb5a328613c3f0016d33fefa9
BagusSemesta2407 <semestabagus24@gmail.com>
2024-01-08 13:43:20 +0700

    unit test continue

--- tests/Feature/TodolistControllerTest.php
+++ tests/Feature/TodolistControllerTest.php
@@ -36,4 +36,31 @@ class TodolistControllerTest extends TestCase
         ])->post('/todolist', [])
         ->assertSeeText('Todo is required');
     }
+
+    public function testAddTodoSuccess()
+    {
+        $this->withSession([
+            'user' => 'bagus',
+        ])->post('/todolist', [
+            'todo' => 'todo'
+        ])->assertRedirect('/todolist');
+    }
+
+    public function testRemoveTodolist()
+    {
+        $this->withSession([
+            'user' => 'bagus',
+            'todolist' => [
+                [
+                    'id'  => '1',
+                    'todo' => 'bagus',
+                ],
+                [
+                    'id'  => '2',
+                    'todo' => 'semesta',
+                ],
+            ]
+        ])->post('/todolist/1/delete')
+        ->assertRedirect('/todolist');
+    }
 }
